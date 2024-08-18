da2256c8f4b9681babf0961f4ee48b2d34d15715
monica230 <monicamaltacouto@gmail.com>
2024-08-18 17:40:14 -0300

    tabelas commit

--- cadastropts.js
+++ cadastropts.js
@@ -1,26 +1,89 @@
 function adicionarPet(event) {
-    console.log("entrando na função")
-    event.preventDefauly()// evita da tela de recarregar
-    const foto = document.getElementById("foto").value}
-    // conta e uma variavel não muda,  document é a minha tela getElementById e pra ir lá na tela e pegar o documento  que tem(id) , o value é o valor.
-    const nome = document.getElementById("nome").value
-    const idade = document.getElementById("idade").value
-    const cor = document.getElementById("cor").value
-    const descrição = document.getElementById("descrição").value
-    const tipo = document.getElementById("tipo").value
+
+
+    /* ======= Capturou os valores do formulario  ======= */
+
+    event.preventDefault() // evita da tela recarregar :)
+
+    const foto = document.getElementById('foto').value
+    const nome = document.getElementById('nome').value
+    const idade = document.getElementById('idade').value
+    const cor = document.getElementById('cor').value
+    const descricao = document.getElementById('descricao').value
+    const tipo = document.getElementById('tipo').value
+
+    /* ======= VALIDACAO DO FORMULARIO  ======= */
+
+    if (foto === "") {
+        document.getElementById('foto').style.borderColor = "red"
+        document.getElementById('foto').style.borderWidth = "2px"
+        document.getElementById('error-foto').innerText = "Foto do pet é obrigatória"
+    } else {
+        document.getElementById('foto').style.borderColor = ""
+        document.getElementById('foto').style.borderWidth = ""
+        document.getElementById('error-foto').innerText = ""
+    }
 
     if (nome === "") {
-        //alert("Nome e obrigatorio")//
-        document.getElementById("nome").style.borderColor = "red"
-        document.gedElementById("nome").style.borderwidth = "2px"
-        documento.getElementById("error-nome").ineerText = "Nome é obrigatório"
+        // alert("Nome é obrigatório") Mostra um aviso no navegaor
+        document.getElementById('nome').style.borderColor = "red"
+        document.getElementById('nome').style.borderWidth = "2px"
+        document.getElementById('error-nome').innerText = "Nome é obrigatório"
+    } else {
+        document.getElementById('nome').style.borderColor = ""
+        document.getElementById('nome').style.borderWidth = ""
+        document.getElementById('error-nome').innerText = ""
+    }
+
+    if (descricao === "") {
+        document.getElementById('descricao').style.borderColor = "red"
+        document.getElementById('descricao').style.borderWidth = "2px"
+        document.getElementById('error-descricao').innerText = "Descricao é obrigatória"
+    } else {
+        document.getElementById('descricao').style.borderColor = ""
+        document.getElementById('descricao').style.borderWidth = ""
+        document.getElementById('error-descricao').innerText = ""
+    }
+
+    if (tipo === "") {
+        document.getElementById('tipo').style.borderColor = "red"
+        document.getElementById('tipo').style.borderWidth = "2px"
+        document.getElementById('error-tipo').innerText = "O tipo é obrigatório"
+    } else {
+        document.getElementById('tipo').style.borderColor = ""
+        document.getElementById('tipo').style.borderWidth = ""
+        document.getElementById('error-tipo').innerText = ""
     }
 
 
-/*1 - colocar uma tag select
-/* 2 - colocar uma data
+    if (nome && foto && descricao && tipo) {
+        const pet = {
+            // id: Math.random() Opcão 1 para gerar um número aleatório,
+            // id: crypto.randomUUID() Opção 2 para gerar um número aleatório,
+            id: Date.now(),
+            foto: foto,
+            nome: nome,
+            idade: idade,
+            cor: cor,
+            descricao: descricao,
+            tipo: tipo,
+            // criado: new Date().toLocaleDateString()
+        }
 
+        let listaNoLocalStorage = JSON.parse(localStorage.getItem("pets"))    // vai no local storage e pega a lista
 
-document //seu documento html
-    .getElementById("form-pet")// ir no documento localizar elemento
-    .addEventListener("subimt", adicionarPet)//Adicionar um evento de sbmit vinculado a função adicionar 
+        if (listaNoLocalStorage === null) listaNoLocalStorage = []
+
+        listaNoLocalStorage.push(pet)
+
+        localStorage.setItem("pets", JSON.stringify(listaNoLocalStorage)) // salvar no local storage
+
+        document.getElementById('form-pet').reset()
+
+    }
+    
+}
+
+document // seu documento HTML
+    .getElementById('form-pet') // ir no documento e localizar o elemento com id form-pet
+    .addEventListener('submit', adicionarPet) // adicionar um evento de submissão vinculado a funcao adicionar pet
---
+++ scrpts/listagemPets.js
@@ -0,0 +1,54 @@
+function deletar(idRecebido) {
+    // 1 -  ir no local e busca o array de pets
+    const petsAtuaisNaMemoria = JSON.parse(localStorage.getItem('pets'))
+    const petsFiltrados = petsAtuaisNaMemoria.filter((item) => item.id !== idRecebido)
+
+    localStorage.setItem('pets', JSON.stringify(petsFiltrados))
+
+    document.getElementById('lista-pets').innerText = ''
+    carregarDados()
+}
+
+function carregarDados() {
+    console.log("chamei a funcao")
+    const petsNaMemoria = JSON.parse(localStorage.getItem('pets'))
+
+    const lista = document.getElementById('lista-pets')
+
+    petsNaMemoria.forEach((pet) => {
+
+
+        const div = document.createElement('div')
+        div.classList.add("item-pet")
+
+        const img = document.createElement('img')
+        img.setAttribute("width", "150px")
+        img.setAttribute("height", "150px")
+        //img.style.objectFit = "cover"
+        img.setAttribute('src', pet.foto)
+
+        div.append(img)
+
+        const h2 = document.createElement('h2')
+        h2.innerText = pet.nome
+
+        div.append(h2)
+
+        const button = document.createElement('button')
+        button.innerText = "Deletar"
+        button.onclick = () => deletar(pet.id)
+
+        div.append(button)
+
+        /*  FIM Geração da div */
+
+        lista.append(div)
+    })
+
+    /* Geração da div */
+
+
+}
+
+
+document.addEventListener('DOMContentLoaded', carregarDados) // quando for renderiza , vai disparar a funcao
